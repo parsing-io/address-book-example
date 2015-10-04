eebb7902fbbb1b6febb3c245512462878b9f5b9b
petrfiser <petr.fiser.9@gmail.com>
2015-10-04 21:56:51 +0200

    removing tentative implementatio

--- src/main/scala/addressbook/Person.scala
+++
@@ -1,9 +0,0 @@
-package addressbook
-
-import java.time.LocalDate
-
-/**
- *
- */
-case class Person(name: String, sex: String, birth: LocalDate)
-
--- src/main/scala/addressbook/Util.scala
+++ src/main/scala/addressbook/Util.scala
@@ -6,12 +6,15 @@ import java.time.format.DateTimeFormatter
 import com.typesafe.config.{Config, ConfigFactory}
 
 /**
-  *
-  */
+ * Utils for configuration and type conversion.
+ */
 object Util {
 
-    val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yy")
+    val conf: Config = ConfigFactory.load()
+
+    // date and time
+    val dateTimeFormatter = DateTimeFormatter.ofPattern(conf.getString("exercise.parsing.datePattern"))
     def stringToDate(date: String): LocalDate = LocalDate.parse(date, dateTimeFormatter)
     def dateToString(date: LocalDate): String = dateTimeFormatter.format(date)
 
- }
+}
